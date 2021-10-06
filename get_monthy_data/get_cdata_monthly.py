163b4671fce4b0b6733f9fff1bf98a07b5c7c452
barganor <ganorbar@gmail.com>
2021-10-06 18:07:09 +0300

    cdata monthly complete

--- get_monthy_data/get_cdata_monthly.py
+++ get_monthy_data/get_cdata_monthly.py
@@ -28,6 +28,7 @@ def cols_d_to_h():
         print('problem getting cols: D-H')
 
 def cols_i_to_k():
+    try:
         current_month = datetime.today().month
         current_year = datetime.today().year
 
@@ -71,8 +72,9 @@ def cols_i_to_k():
                     current_month += 1
             result_df.index = temp
         return result_df.astype('int64')
+    except:
+        print('problem getting cols: I-K')
 
-print(cols_i_to_k())
 def col_l():
     try:
         url = 'https://www.boi.org.il/Lists/BoiChapterTablesFiles/d010.xls'
