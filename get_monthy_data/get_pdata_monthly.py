3f15aaa338fbd3e5cc150880e720381c95707b11
אליה פיקאר <eliya688@gmail.com>
2022-02-28 20:15:20 +0200

    update get_pdata_monthly.py

--- get_monthy_data/get_pdata_monthly.py
+++ get_monthy_data/get_pdata_monthly.py
@@ -3,28 +3,55 @@ import pandas as pd
 from datetime import datetime, date, timedelta
 from bs4 import BeautifulSoup
 from dateutil.relativedelta import relativedelta
-import time
+import string
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 
 
-def cols_d_to_e_mdata():
-    now = date.today()
-    year = now.year
-    past_date = now - relativedelta(years=2)
-    past_date_year = past_date.year
-    year_list = [str(year) for year in range(past_date_year, year + 1, 1)]
-    df = pd.DataFrame()
+def cols_g_to_t_pdata():
     try:
-        for year in year_list:
-            url = f'https://home.treasury.gov/resource-center/data-chart-center/interest-rates/daily-treasury-rates.csv/{year}/all?type=daily_treasury_yield_curve&field_tdr_date_value={year}&page&_format=csv'
-            df1 = pd.read_csv(url, index_col=0)
-            df1 = df1.loc[:,["1 Yr", "10 Yr"]]
-            df1 = df1.groupby(pd.PeriodIndex(df1.index, freq="M")).mean()
-            df = pd.concat([df, df1], axis=0)
-        df = df.sort_index()
-        df.index = df.index.strftime('%m/%Y')
-        return df
+        series_id = {'col_g': '120010', 'col_h': '120040', 'col_i': '120020', 'col_j': '120030',
+                     'col_k': '120450', 'col_l': '120520','col_m': '120660','col_n': '120850','col_o': '121050'
+                    ,'col_p': '121230'
+                    ,'col_q': '121320','col_r': '121430','col_s': '120050','col_t': '120043'}
+        df = pd.DataFrame()
+        for id in series_id.keys():
+            url = f'https://api.cbs.gov.il/index/data/price?id={series_id.get(id)}&format=json&download=false'
+            resp = requests.get(url).json()
+            data = resp['month'][0]['date']
+            col = pd.DataFrame.from_records(data)
+            col['E'] = col['currBase'].apply(pd.Series).value
+            col['Date'] =  col['month'].map(str) + '/' + col['year'].map(str)
+            col = col[['Date','E']]
+            col = col.set_index('Date')
 
+            col.index = pd.to_datetime(col.index)
+            col = col.sort_index()
+            col.index = col.index.strftime('%m/%Y')
+            df = pd.concat([df, col], axis=1)
+        df.columns = ['מדד המחירים לצרכן-כללי(מקור)', 'מדד לצרכן- ירקות ופירות', 'מדד מחירים לצרכן - ללא ירקות ופירות', 'מדד לצרכן - מדד המחירים לצרכן - המדד הכללי ללא ירקות ופירות וללא דיור',
+                     'מדד לצרכן - דיור', 'מדד לצרכן - אחזקת הדירה','מדד לצרכן - ריהוט וציוד לבית','מדד לצרכן - הלבשה והנעלה','מדד לצרכן - חינוך, תרבות ובידור','מדד לצרכן - בריאות'
+                    ,'מדד לצרכן - תחבורה ותקשורת','מדד לצרכן - שונות','מדד לצרכן - מזון ללא ירקות ופירות','מדד לצרכן - ירקות ופירות - פירות טריים']
+        return df
     except Exception as e:
-        print('problem getting cols: D-E' + str(e))
+        print('problem getting col: G-T ' + str(e))
+
+
+# Empty columns
+def cols_u_to_v_pdata():
+    try:
+        today = datetime(date.today().year, date.today().month,
+                                 date.today().day).strftime('%m/%Y')
+        dtObj = datetime.strptime(today, '%m/%Y')
+        index_col = [(dtObj - relativedelta(months=i)).date() for i in range(1, 30, 1)]
+        cols_name = []
+        for first_char in string.ascii_uppercase:
+            if first_char <= 'V' and first_char >= 'U':
+                cols_name.append(first_char)
+
+        df = pd.DataFrame(columns=cols_name, index=index_col)
+        df = df.fillna("").sort_index()
+        df.index = pd.to_datetime(df.index).strftime('%m/%Y')
+        return df
+    except Exception as e:
+        print('problem getting col: U-V' + str(e))
